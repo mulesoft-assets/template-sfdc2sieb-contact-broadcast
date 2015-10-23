014b59852a59a1440bb63b41c7c71e53a1660a5d
Tomas Verescak <hotovo.tomas@hotovo.org>
2015-10-23 12:04:07 +0200

    SIT-1425 Migration to 3.7.2

--- src/test/java/org/mule/templates/integration/BusinessLogicIT.java
+++ src/test/java/org/mule/templates/integration/BusinessLogicIT.java
@@ -30,7 +30,7 @@ import com.mulesoft.module.batch.BatchTestHelper;
  */
 public class BusinessLogicIT extends AbstractTemplateTestCase {
 
-	protected static final int TIMEOUT = 60;
+	protected static final int TIMEOUT = 240;
 	private static final String POLL_FLOW_NAME = "triggerFlow";
 	private static final String ACCOUNT_NAME = "Contact Broadcast Test Name";
 	
@@ -47,8 +47,9 @@ public class BusinessLogicIT extends AbstractTemplateTestCase {
 	
 	@BeforeClass
 	public static void init() {
-		System.setProperty("polling.frequency", "10000");
-		System.setProperty("polling.startDelayMillis", "20000");
+		System.setProperty("poll.frequencyMillis", "10000");
+		System.setProperty("account.sync.policy", "syncAccount");
+		System.setProperty("poll.startDelayMillis", "20000");
 		System.setProperty("watermark.default.expression",
 				"#[groovy: new Date(System.currentTimeMillis() - 10000).format(\"yyyy-MM-dd'T'HH:mm:ss.SSS'Z'\", TimeZone.getTimeZone('UTC'))]");
 	}
@@ -102,6 +103,7 @@ public class BusinessLogicIT extends AbstractTemplateTestCase {
 	@Test
 	@SuppressWarnings("unchecked")
 	public void testMainFlow() throws Exception {
+		
 		// Run poll and wait for it to run
 		runSchedulersOnce(POLL_FLOW_NAME);
 		waitForPollToRun();
@@ -109,7 +111,7 @@ public class BusinessLogicIT extends AbstractTemplateTestCase {
 		// Wait for the batch job executed by the poll flow to finish
 		helper.awaitJobTermination(TIMEOUT * 1000, 500);
 		helper.assertJobWasSuccessful();
-
+		
 		// Execute selectAccountFromSiebel sublow
 		MuleEvent event = selectAccountFromSiebelFlow.process(getTestEvent(account, MessageExchangePattern.REQUEST_RESPONSE));
 		List<Map<String, Object>> payload = (List<Map<String, Object>>) event.getMessage().getPayload();
@@ -209,9 +211,12 @@ public class BusinessLogicIT extends AbstractTemplateTestCase {
 				.with("LastName", buildUniqueName(TEMPLATE_NAME, "LastName_"))
 				.with("Email", buildUniqueEmail("some.email." + sequence))
 				.with("Description", "Some fake description")
-				.with("MailingCity", "Denver").with("MailingCountry", "US")
+				.with("MailingCity", "Denver")
+				.with("MailingCountry", "US")
 				.with("MobilePhone", "123456789")
 				.with("Department", "department_" + sequence)
-				.with("Phone", "123456789").with("Title", "Dr").build();
+				.with("Phone", "123456789")
+				.with("Title", "Dr")
+				.build();
 	}
 }
